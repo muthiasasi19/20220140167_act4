bb8c7b5c33bd1dd3254ed539ba6c2c59a26b93d1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-04-23 13:05:07 +0700

    Tambah navigasi setelah validasi berhasil

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -53,6 +53,16 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
+               ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                },
+                ch
 
 
 
