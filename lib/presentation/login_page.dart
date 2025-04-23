fb56d9cc80b0392413109557ccba11d63c9eb117
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-04-23 12:46:18 +0700

    first commit

---
+++ lib/presentation/login_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
+import 'package:play_navigation/presentation/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
