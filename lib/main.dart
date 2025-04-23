9d943c61d250d38f6ec8a812a3e5eb2aa0609412
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-04-23 14:19:18 +0700

    Mengatur routing aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
+import 'package:play_navigation/presentation/order_page.dart';
+import 'package:play_navigation/presentation/login_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/order': (context) => const OrderPage(),
+      },
+    );
+  }
+}
